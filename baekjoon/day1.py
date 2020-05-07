7f50583c551c544e51a678c5d0557c9f9d74e8d4
SungJae <50938270+bernow@users.noreply.github.com>
2020-05-07 18:00:41 +0900

    0507
    python_baekjoon

---
+++ baekjoon/day1.py
@@ -0,0 +1,19 @@
+# 문제1 
+# print("Hello World!")
+
+# 문제2
+# print("\    /\ \n )  ( ') \n(  /  ) \n \(__)|")
+
+# 문제3
+# a, b = input().split()
+# a = int(a)
+# b = int(b)
+# print(a+b)
+
+# 문제4
+# a, b = input().split()
+# print(int(a)-int(b))
+
+# 문제5
+# a, b = input().split()
+# print(int(a)*int(b))
